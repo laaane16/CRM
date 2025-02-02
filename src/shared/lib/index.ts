dc2b1c217145213ed25fbe424d75623ce34879d9
laaane16 <nikon200402@gmail.com>
2025-02-02 19:27:32 +0300

    fix: error in reexport

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,3 +1,2 @@
-export { TestAsyncThunk } from './tests/TestAsyncThunk';
 export { default as StyleDecorator } from './decorators/StyleDecorator';
 export { default as DynamicModuleLoader } from './components/DynamicModuleLoader';
