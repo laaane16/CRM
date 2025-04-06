0905226e7da8c80b05b4efed83aa9dbfae986b0e
laaane16 <nikon200402@gmail.com>
2025-04-06 21:38:35 +0300

    fix: set correct args for buildSelector

--- src/shared/lib/store/buildSelector.ts
+++ src/shared/lib/store/buildSelector.ts
@@ -1,12 +1,13 @@
 import { useSelector } from 'react-redux';
 import { StateSchema } from '../../../app/providers';
 
-type ISelector<T> = (state: StateSchema) => T;
-type Result<T> = [() => T, ISelector<T>];
+type ISelector<T, Args extends unknown[]> = (state: StateSchema, ...args: Args) => T;
+type Hook<T, Args extends unknown[]> = (...args: Args) => T;
+type Result<T, Args extends unknown[]> = [Hook<T, Args>, ISelector<T, Args>];
 
-export const buildSelector = <T>(selector: ISelector<T>): Result<T> => {
-  const useSelectorHook = () => {
-    return useSelector(selector);
+export const buildSelector = <T, Args extends unknown[]>(selector: ISelector<T, Args>): Result<T, Args> => {
+  const useSelectorHook: Hook<T, Args> = (...args) => {
+    return useSelector((state: StateSchema) => selector(state, ...args));
   };
 
   return [useSelectorHook, selector];
